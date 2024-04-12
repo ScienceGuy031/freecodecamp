57c859765626ae5ef4de903a100241f26b8a7937
benjamin.kurtisi <benjamin.kurtisi@tritum.de>
2024-04-12 16:33:16 +0200

    navlinks for every project

--- script.js
+++ script.js
@@ -55,45 +55,64 @@ const certifications = [
 ];
 
 // NAVBAR
-const navLeft = document.getElementById('nav-list');
+const navLeft = document.getElementById("nav-list");
 
 certifications.forEach((cert) => {
-  const navLi = document.createElement('li');
-  const navLink = document.createElement('a');
-  navLink.href = `#${cert.name.replace(' ', '-')}`;
-  navLink.innerText = cert.name;
-  navLi.classList.add('nav-item');
-  navLink.classList.add('nav-link');
-  navLi.appendChild(navLink);
-  navLeft.appendChild(navLi);
+  const dropdownItem = document.createElement('li');
+  dropdownItem.className = 'nav-item dropdown';
+  const dropdownList = document.createElement("ul");
+  dropdownList.classList.add("dropdown-menu");
+  const dropdownToggle = document.createElement("a");
+  dropdownToggle.className = "nav-link dropdown-toggle";
+  dropdownToggle.href = "#";
+  dropdownToggle.role = "button";
+  dropdownToggle.ariaExpanded = false;
+  dropdownToggle.innerText = cert.name;
+  const dataBsToggle = document.createAttribute("data-bs-toggle");
+  dataBsToggle.value = "dropdown";
+  dropdownToggle.setAttributeNode(dataBsToggle);
+  dropdownItem.appendChild(dropdownToggle);
+  dropdownItem.appendChild(dropdownList);
+  navLeft.appendChild(dropdownItem);
+
+  cert.projects.forEach((project) => {
+    const navLi = document.createElement("li");
+    const navLink = document.createElement("a");
+    navLink.href = `#${project.name.replace(/ /g, "-")}`;
+    navLink.innerText = project.name;
+    navLink.classList.add("dropdown-item");
+    navLi.appendChild(navLink);
+    dropdownList.appendChild(navLi);
+  });
 });
 
 // SECTIONS
 
 certifications.forEach((cert) => {
-  const section = document.createElement('section')
+  const section = document.createElement("section");
   root.appendChild(section);
-  section.id = cert.name.replace(' ', '-');
-  const sectionHeadline = document.createElement('h2');
+  section.id = cert.name.replace(/ /g, "-");
+  const sectionHeadline = document.createElement("h2");
   section.appendChild(sectionHeadline);
-  section.appendChild(document.createElement('hr'));
+  section.appendChild(document.createElement("hr"));
   sectionHeadline.innerText = cert.name;
-  section.classList.add('text-center');
+  section.classList.add("text-center");
 
   cert.projects.forEach((project) => {
-    const projectHeadline = document.createElement('h3');
-    const projectLink = document.createElement('a');
+    const projectHeadline = document.createElement("h3");
+    projectHeadline.id = project.name.replace(/ /g, "-");
+    const projectLink = document.createElement("a");
     projectHeadline.appendChild(projectLink);
     projectLink.href = project.url;
-    projectLink.target = '_blank';
+    projectLink.target = "_blank";
     projectLink.innerText = project.name;
     section.appendChild(projectHeadline);
 
-    const frameContainer = document.createElement('div');
-    const iframe = document.createElement('iframe');
+    const frameContainer = document.createElement("div");
+    const iframe = document.createElement("iframe");
     frameContainer.appendChild(iframe);
     section.appendChild(frameContainer);
     iframe.src = project.url;
-    frameContainer.className = 'container';
-  })
-})
+    frameContainer.className = "container";
+  });
+});
